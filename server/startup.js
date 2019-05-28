b6f459fc5d0b4342ea34c5f2ff511e028f19221f
Benja <muimuiasd@gmail.com>
2019-05-28 12:20:22 -0400

    Inicialización de la aplicación
    Startup incluye la creación de un administrador de prueba y una colección de flores.
    
    La colección se ha generado desde la clase collection.js

---
+++ server/startup.js
@@ -0,0 +1,30 @@
+Meteor.startup(function()
+{
+    if (Meteor.users.find().count() == 0)
+    {
+        Accounts.createUser({
+            username: 'admin',
+            email: 'admin@test.com',
+            password: 'test',
+            profile: {
+                name: 'Test Admin'
+            }
+        });
+    }
+    if (Flowers.find.count() == 0)
+    {
+        flowerArray.forEach(function(flower) {
+            Flowers.insert(flower);
+        })
+    }
+});
+
+var flowerArray = [
+    {
+        flower: "Margarita 001",
+        category: "Margaritas"
+    },
+    {
+        flower: "Margarita 002",
+        category: "Margaritas"
+    }];
