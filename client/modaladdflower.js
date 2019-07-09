d80489aeed1bede10056b4e37b7d1a8b0818190f
Benja <muimuiasd@gmail.com>
2019-07-09 14:40:31 -0400

    Auto stash before merge of "Desarrollo" and "origin/Desarrollo"

--- client/modaladdflower.js
+++ client/modaladdflower.js
@@ -11,5 +11,40 @@ Template.modaladdflower.helpers({
             };
             return cat;
         });
+    },
+    flower()
+    {
+
+        let doc = {};
+        let img;
+        doc.img = img ? img.link() : "img/flower.png";
+        return doc;
+    }
+});
+
+Template.modaladdflower.events({
+    "click #btnG": function(e)
+    {
+        let doc = {};
+        let flower = Session.get("ActualFlower") ? Session.get("ActualFlower") : {};
+        let name = $("#input-flower-name").val();
+        let description = $("#input-flower-description").val();
+        let precio= $("#input-flower-Price").val();
+        let categorias = Session.get("CategoriasFlor") ? Session.get("CategoriasFlor") : [];
+        doc.name = name;
+        doc.description = description;
+        doc.precio=precio;
+        doc.categorias = categorias;
+        Meteor.call("AddFlower", flower ? flower._id : false, doc, function (err, resp)
+        {
+            if (!err)
+            {
+                if (!flower._id)
+                {
+                    flower._id = resp;
+                }
+            } else console.warn(err);
+        });
+        $("#modal-ingreso-flor").hide();
     }
 });
